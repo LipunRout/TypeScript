e6ce5c70a11dad167ffc1f8a07cba227e194d271
LIPUN ROUT <lipunrout001@gmail.com>
2026-04-20 18:11:35 +0530

    update

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let a = 12;
 let b = "c";
+var SatusCode;
+(function (SatusCode) {
+    SatusCode[SatusCode["ABANDONED"] = 500] = "ABANDONED";
+    SatusCode[SatusCode["BANED"] = 404] = "BANED";
+})(SatusCode || (SatusCode = {}));
+console.log(SatusCode.BANED);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 let a=12;
 
 let b="c";
+
+
+enum SatusCode{
+  ABANDONED=500,
+  BANED=404,
+}
+console.log(SatusCode.BANED);
+
