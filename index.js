ac3d1ee24234e386c985be451c3da69b36334aee
LIPUN ROUT <lipunrout001@gmail.com>
2026-04-21 18:31:54 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
 "use strict";
+// let a=12;
 Object.defineProperty(exports, "__esModule", { value: true });
-let a = 12;
-let b = "c";
-var SatusCode;
-(function (SatusCode) {
-    SatusCode[SatusCode["ABANDONED"] = 500] = "ABANDONED";
-    SatusCode[SatusCode["BANED"] = 404] = "BANED";
-})(SatusCode || (SatusCode = {}));
-console.log(SatusCode.BANED);
+// let b="c";
+// enum SatusCode{
+//   ABANDONED=500,
+//   BANED=404,
+// }
+// console.log(SatusCode.BANED);
+// interface User{
+//   name:string;
+//   age:number;
+//   isAdmin:boolean;
+// }
+// function getUser(Obj : User){
+//   console.log(Obj.name);
+//   console.log(Obj.age);
+//   console.log(Obj.isAdmin);
+// }
+// getUser({name:"John",age:30,isAdmin:true});
+// const obj={
+//   name:"Lipun",
+//   age:23,
+//   isAdmin:true,
+//   role:"SOFTWARE ENGINEER"
+// }
+// interface User{
+//   name:string;
+//   age:number;
+//   isAdmin:boolean;
+// }
+// interface Admin extends User{
+//   role?:string;
+// }
+// function getUser(ov:Admin){
+//   console.log(ov.name);
+//   console.log(ov.age);
+//   console.log(ov.isAdmin);
+//   console.log(ov.role);
+//   ov.role="Admin";
+// }
+// getUser(obj);
+//UNION TYPE
+// type Val=number | string | boolean;
+// let a :Val;
+// a=true;
+// console.log(a);
+//Intersection Type
+// type A = { name: string };
+// type B = { age: number };
+// type Person = A & B;
+// let p: Person = {
+//   name: "Lipun",
+//   age: 21
+// };
+class Person {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        console.log(`Hello, my name is ${this.name}`);
+    }
+    Age() {
+        console.log(`I am ${this.age} years old`);
+    }
+}
+let person1 = new Person("Lipun", 21);
+person1.greet();
+person1.Age();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,11 +1,96 @@
-let a=12;
+// let a=12;
 
-let b="c";
+// let b="c";
 
 
-enum SatusCode{
-  ABANDONED=500,
-  BANED=404,
+// enum SatusCode{
+//   ABANDONED=500,
+//   BANED=404,
+
+// }
+// console.log(SatusCode.BANED);
+// interface User{
+//   name:string;
+//   age:number;
+//   isAdmin:boolean;
+// }
+
+
+// function getUser(Obj : User){
+//   console.log(Obj.name);
+//   console.log(Obj.age);
+//   console.log(Obj.isAdmin);
+// }
+// getUser({name:"John",age:30,isAdmin:true});
+
+
+
+// const obj={
+//   name:"Lipun",
+//   age:23,
+//   isAdmin:true,
+//   role:"SOFTWARE ENGINEER"
+// }
+// interface User{
+//   name:string;
+//   age:number;
+//   isAdmin:boolean;
+// }
+// interface Admin extends User{
+//   role?:string;
+// }
+
+
+// function getUser(ov:Admin){
+//   console.log(ov.name);
+//   console.log(ov.age);
+//   console.log(ov.isAdmin);
+//   console.log(ov.role);
+//   ov.role="Admin";
+  
+// }
+// getUser(obj);
+
+
+
+//UNION TYPE
+// type Val=number | string | boolean;
+
+// let a :Val;
+
+// a=true;
+// console.log(a);
+
+
+//Intersection Type
+
+// type A = { name: string };
+// type B = { age: number };
+
+// type Person = A & B;
+
+// let p: Person = {
+//   name: "Lipun",
+//   age: 21
+// };
+
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet(): void {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+  Age(): void {
+    console.log(`I am ${this.age} years old`);
+  } 
 }
-console.log(SatusCode.BANED);
-
+let person1 = new Person("Lipun", 21);
+person1.greet();
+person1.Age();
